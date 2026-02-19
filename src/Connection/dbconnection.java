26f382d7247a0c9a064dcab1bc04a111c59e9521
nayan-pokharel03 <pokharelnayan03@gmail.com>
2026-02-19 21:14:37 +0545

    Database

--- src/Connection/dbconnection.java
+++ src/Connection/dbconnection.java
@@ -1,4 +1,19 @@
 package Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class dbconnection {
+
+        public static Connection getConnection() throws SQLException {
+            Connection connection = null;
+            String URL = "jdbc:mysql://localhost:3306/crud";
+            String USER = "root";
+            String PASSWORD = "";
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            return connection;
+        }
 }
+
+
