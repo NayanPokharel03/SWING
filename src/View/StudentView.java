13b5d768a796633426b9b5317bee4544642f94aa
nayan-pokharel03 <pokharelnayan03@gmail.com>
2026-03-12 15:27:56 +0545

    Update StudentView.java
    final ui improvements in student view
    changing rrollno to phone no
    fixing visibility issue and updating buttons

--- src/View/StudentView.java
+++ src/View/StudentView.java
@@ -1,120 +1,174 @@
 package View;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 import java.awt.*;
 
 public class StudentView extends JFrame {
 
 
+     public JTextField first_name;
+     public JTextField last_name;
+     public JTextField phone_no;
+     public JTextField email;
+     public JTextField gender;
+     public JTextField dob;
+     public JTextField age;
+     public JTextField address;
+
+     public JButton add;
+     public JButton delete;
+     public JButton edit;
+     public JButton reset;
+
+
+     public DefaultTableModel tableModel;
+     public JTableHeader tableHeader;
+     public JTable table;
+
        public StudentView() {
 
             // Create frame
-            JFrame frame = new JFrame("Student Details");
-            frame.setSize(1300, 900);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setLayout(null);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+            setTitle("Student Details");
+            setSize(1300, 900);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setLayout(null);
+            setLocationRelativeTo(null);
+
 
             //Font Details
             Font font = new Font("Arial", Font.BOLD, 18);
 
             //Text and font details
             JLabel label = new JLabel("First Name:");
-            label.setBounds(10, 50, 1000, 18);
+            label.setBounds(20, 20, 120, 25);
             label.setFont(font);
-            frame.add(label);
+            add(label);
+
 
             label = new JLabel("Last Name:");
-            label.setBounds(10, 75, 1000, 18);
+            label.setBounds(20, 55, 120, 25);
             label.setFont(font);
-            frame.add(label);
+            add(label);
 
-            label = new JLabel("Roll no:");
-            label.setBounds(10, 100, 1000, 18);
+            label = new JLabel("Phone No:");
+            label.setBounds(20, 90, 120, 25);
             label.setFont(font);
-            frame.add(label);
+            add(label);
 
             label = new JLabel("Email:");
-            label.setBounds(10, 125, 1000, 18);
+            label.setBounds(20, 125, 120, 25);
             label.setFont(font);
-            frame.add(label);
+            add(label);
 
             label = new JLabel("Age:");
-            label.setBounds(10, 150, 1000, 22);
+            label.setBounds(20, 160, 120, 25);
             label.setFont(font);
-            frame.add(label);
+            add(label);
 
             label = new JLabel("Gender:");
-            label.setBounds(10, 175, 1000, 18);
+            label.setBounds(20, 195, 120, 25);
             label.setFont(font);
-            frame.add(label);
+            add(label);
 
             label = new JLabel("Date of Birth:");
-            label.setBounds(10, 200, 1000, 18);
+            label.setBounds(20, 230, 120, 25);
             label.setFont(font);
-            frame.add(label);
+            add(label);
 
             label = new JLabel("Address:");
-            label.setBounds(10, 225, 1000, 18);
+            label.setBounds(20, 265, 120, 25);
             label.setFont(font);
-            frame.add(label);
+            add(label);
+
+
+          // Text Fields
+            first_name = new JTextField();
+            first_name.setBounds(160, 20, 250, 25);
+            add(first_name);
+
+            last_name = new JTextField();
+            last_name.setBounds(160, 55, 250, 25);
+            add(last_name);
+
+            phone_no = new JTextField();
+            phone_no.setBounds(160, 90, 250, 25);
+            add(phone_no);
+
+            email = new JTextField();
+            email.setBounds(160, 125, 250, 25);
+            add(email);
+
+            age = new JTextField();
+            age.setBounds(160, 160, 250, 25);
+            add(age);
+
+            gender = new JTextField();
+            gender.setBounds(160, 195, 250, 25);
+            add(gender);
+
+            dob = new JTextField();
+            dob.setBounds(160, 230, 250, 25);
+            add(dob);
+
+            address = new JTextField();
+            address.setBounds(160, 265, 250, 25);
+            add(address);
+
+
+            //TABLE
+
+            String[] columns = {
+                    "First Name", "Last Name", "Phone No",
+                    "Email", "Age", "Gender", "DOB", "Address"
+            };
+
+            tableModel = new DefaultTableModel(columns, 0);
+            table = new JTable(tableModel);
+
+            // table text styling
+            table.setFont(font);
+            table.setRowHeight(20);
+            table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+
+            // Table header styling
+            JTableHeader header = table.getTableHeader();
+            header.setFont(font);
+            header.setPreferredSize(new Dimension(header.getWidth(), 25));
+
+            // Scroll pane
+            JScrollPane scrollPane = new JScrollPane(table);
+            scrollPane.setBounds(30, 330, 1150, 400);
+            add(scrollPane);
 
             //button
-            JButton button = new JButton("Save");
-            button.setBounds(10, 800, 200, 30);
-            button.setFont(font);
-            frame.add(button);
 
-            JButton button1 = new JButton("Cancel");
-            button1.setBounds(250, 800, 200, 30);
-            button1.setFont(font);
-            frame.add(button1);
+            add = new JButton("Add");
+            add.setBounds(20, 800, 200, 30);
+            add.setFont(font);
+            add(add);
 
-            //Text field
-            JTextField first_name = new JTextField();
-            first_name.setBounds(200, 50, 300, 18);
-            frame.add(first_name);
-            frame.setVisible(true);
+            delete = new JButton("Delete");
+            delete.setBounds(230, 800, 200, 30);
+            delete.setFont(font);
+            add(delete);
 
+            edit = new JButton("Edit");
+            edit.setBounds(440, 800, 200, 30);
+            edit.setFont(font);
+            add(edit);
 
-            JTextField last_name = new JTextField();
-            last_name.setBounds(200, 75, 300, 18);
-            frame.add(last_name);
-            frame.setVisible(true);
-
-            JTextField roll_no = new JTextField();
-            roll_no.setBounds(200, 100, 300, 18);
-            frame.add(roll_no);
-            frame.setVisible(true);
-
-            JTextField email = new JTextField();
-            email.setBounds(200, 125, 300, 18);
-            frame.add(email);
-            frame.setVisible(true);
-
-            JTextField gender = new JTextField();
-            gender.setBounds(200, 150, 300, 18);
-            frame.add(gender);
-            frame.setVisible(true);
-
-            JTextField age = new JTextField();
-            age.setBounds(200, 175, 300, 18);
-            frame.add(age);
-            frame.setVisible(true);
-
-            JTextField dob = new JTextField();
-            dob.setBounds(200, 200, 300, 18);
-            frame.add(dob);
-            frame.setVisible(true);
-
-            JTextField address = new JTextField();
-            address.setBounds(200, 225, 300, 18);
-            frame.add(address);
-            frame.setVisible(true);
+            reset = new JButton("Reset");
+            reset.setBounds(650, 800, 200, 30);
+            reset.setFont(font);
+            add(reset);
 
 
-        }
+
+            setVisible(true);
+
+       }
     public static void main(String[] args){
            new StudentView();
     }
