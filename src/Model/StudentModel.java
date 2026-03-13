579e1ee14da9c85cfe23b2997a67ad68879ac314
nayan-pokharel03 <pokharelnayan03@gmail.com>
2026-03-13 08:22:10 +0545

    Update StudentModel.java

--- src/Model/StudentModel.java
+++ src/Model/StudentModel.java
@@ -1,16 +1,97 @@
 package Model;
 
+import javax.swing.*;
+import java.util.Date;
+
 public class StudentModel {
 
-    private String firstName;
-    private String lastName;
-    private String RollNo;
-    private String email;
-    private int age;
-    private String gender;
-    private String dateOfBirth;
-    private String address;
+    private String  first_name;
+    private String  last_name;
+    private String  phone_no;
+    private String  email;
+    private String  gender;
+    private Date  dob;
+    private String  age;
+    private String  address;
 
+    public StudentModel() {
+    }
 
+    public StudentModel(String first_name, String last_name, String phone_no, String email,
+                        String gender, Date dob, String age, String address) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone_no = phone_no;
+        this.email = email;
+        this.gender = gender;
+        this.dob = dob;
+        this.age = age;
+        this.address = address;
+    }
 
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone_no() {
+        return phone_no;
+    }
+
+    public void setPhone_no(String phone_no) {
+        this.phone_no = phone_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
+
+
